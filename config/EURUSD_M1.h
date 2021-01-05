a24ceeb85803f5127a21296ac73b2ff1410d649f
kenorb <kenorb@users.noreply.github.com>
2021-01-05 05:56:26 +0000

    Optimize SignalOpen params (pf=0.18; dd=17.02; tt=15983)

--- config/EURUSD_M1.h
+++ config/EURUSD_M1.h
@@ -1,7 +1,14 @@
-/*
+/**
  * @file
  * Defines default strategy parameter values for the given timeframe.
  */
+
+// Defines indicator's parameter values for the given pair symbol and timeframe.
+struct Indi_AC_Params_M1 : Indi_AC_Params {
+  Indi_AC_Params_M1() : Indi_AC_Params(indi_ac_defaults, PERIOD_M1) {
+    shift = 0;
+  }
+} indi_ac_m1;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
 struct Stg_AC_Params_M1 : StgParams {
@@ -11,11 +18,11 @@ struct Stg_AC_Params_M1 : StgParams {
     signal_open_method = -1;
     signal_open_filter = 0;
     signal_open_level = (float)10;
-    signal_open_boost = 0;
+    signal_open_boost = 1;
     signal_close_method = 1;
     signal_close_level = (float)25;
     price_stop_method = 2;
-    price_stop_level = 100;
+    price_stop_level = (float)25;
     tick_filter_method = 1;
     max_spread = 0;
   }
--- config/EURUSD_M30.h
+++ config/EURUSD_M30.h
@@ -1,21 +1,28 @@
-/*
+/**
  * @file
  * Defines default strategy parameter values for the given timeframe.
  */
+
+// Defines indicator's parameter values for the given pair symbol and timeframe.
+struct Indi_AC_Params_M30 : Indi_AC_Params {
+  Indi_AC_Params_M30() : Indi_AC_Params(indi_ac_defaults, PERIOD_M30) {
+    shift = 0;
+  }
+} indi_ac_m30;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
 struct Stg_AC_Params_M30 : StgParams {
   // Struct constructor.
   Stg_AC_Params_M30() : StgParams(stg_ac_defaults) {
     lot_size = 0;
-    signal_open_method = -1;
+    signal_open_method = -2;
     signal_open_filter = 1;
     signal_open_level = (float)3;
     signal_open_boost = 0;
     signal_close_method = 3;
     signal_close_level = (float)10;
     price_stop_method = 4;
-    price_stop_level = 2;
+    price_stop_level = (float)10;
     tick_filter_method = 1;
     max_spread = 0;
   }
